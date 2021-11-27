052557965e94044862177c7410ee3ac75802f21d
Igor Buyanov <iobuyan1@mts.ru>
2021-11-27 16:21:23 +0300

    [feat] create config

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'siniy_kit.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #DATABASE = '/tmp/flaskr.db'
+    DEBUG = True
+    SECRET_KEY = 'development key'
+    USERNAME = 'admin'
+    PASSWORD = 'default'
+    UPLOAD_FOLDER = '/tmp/'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'json'])
