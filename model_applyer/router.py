07d666ada2f9cee52764f3aeb6170b9bccf99fdc
Igor Buyanov <buyanov.igor.o@yandex.ru>
2022-07-30 13:14:49 +0300

    [feat] added models

--- model_applyer/router.py
+++ model_applyer/router.py
@@ -5,6 +5,9 @@ import logging
 import sys
 from logging import StreamHandler, Formatter
 
+from .ml_models import SuicidalSignalModel
+from .ml_models import SentimentModel
+
 session = Session(bind=engine)
 
 logger = logging.getLogger(__name__)
@@ -13,8 +16,17 @@ handler = StreamHandler(stream=sys.stdout)
 handler.setFormatter(Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
 logger.addHandler(handler)
 
+config # = 
+
+MODELS = {
+    "suicidal_signal": SuicidalSignalModel(config),
+    "sentiment": SentimentModel()
+}
+
 def get_new_user_posts(user_id: int):
     posts = session.query(Posts).where(Posts.user_id == id and Posts.annotation == -1).all()
-
+    for attr_model, model in MODELS.items():
+        labels = model.predict(posts)
+        
     for p in posts:
         p.annotation #=  
