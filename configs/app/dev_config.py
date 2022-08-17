8fe7f77f66d3deef29dff358139c95d2c70f52f1
dvloban <dvloban3@mts.ru>
2022-08-17 17:52:20 +0300

    update gitignote

--- configs/app/dev_config.py
+++ configs/app/dev_config.py
@@ -16,8 +16,10 @@ class DevelopmentConfig(Config):
     COMMON_DATABASE_PATH = f'postgresql://{DB_USER}:{DB_PASS}@{DB_SERVICE}:{DB_PORT}'
     SQLALCHEMY_DATABASE_URI = f'{COMMON_DATABASE_PATH}/{DB_NAME}'
 
-    # CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL',
-    #                                    'redis://localhost:6379'),
-    # CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND',
-    #                                        'redis://localhost:6379')
+    # Redis URL configurations
+    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL',
+                                       'redis://localhost:6379'),
+    CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND',
+                                           'redis://localhost:6379')
+
 
