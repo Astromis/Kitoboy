0a5005f9cfa811e44a84f0d9b2ecbb5846ec548c
dvloban <dvloban3@mts.ru>
2022-09-02 18:35:08 +0300

    Update env settings, enable jupyter notebook

--- start.sh
+++ start.sh
@@ -9,8 +9,12 @@ sudo docker-compose down && sudo docker-compose up -d --build
 # Remove <none> TAG images
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
+# Drop existing db
 docker exec app python3.8 db_manager.py drop_db
 
+# Create fresh db
 docker exec app python3.8 db_manager.py create_db
 
+
+# Fill db with dump data
 docker exec app ./scripts/./db_fill.sh dev
